95bd2280f4753ba93676a46f211c9be3d7d08525
Markfred Chen <markfred.chen@gmail.com>
2019-11-26 17:42:54 +0800

    zipkin tutorial

--- spring/spring-cloud/zipkin/basic/Readme.md
+++
@@ -1,9 +0,0 @@
-# 使用方式
-
-1. 启动 zipkin-server
-```shell script
-docker run -d -p 9411:9411 --name zipkin_server openzipkin/zipkin
-```
-2. 启动`ZipkinBarServiceApplication`
-3. 启动`ZipkinFooServiceApplication`
-4. `sleuth.http` 调用API
--- spring/spring-cloud/zipkin/zipkin-bar-service/src/main/java/io/markfredchen/tech/samples/spring/cloud/zipkin/bar/service/ZipkinBarServiceApplication.java
+++ spring/spring-cloud/zipkin/zipkin-bar-service/src/main/java/io/markfredchen/tech/samples/spring/cloud/zipkin/bar/service/ZipkinBarServiceApplication.java
@@ -2,14 +2,12 @@ package io.markfredchen.tech.samples.spring.cloud.zipkin.bar.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author markfredchen
  * @since 2019/11/26
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ZipkinBarServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinBarServiceApplication.class);
